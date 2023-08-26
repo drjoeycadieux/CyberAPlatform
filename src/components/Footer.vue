10930abba1f81ec0b721877ae7356d48b602af4d
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-08-26 14:15:40 +0000

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts"></script>
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
 
 <template>
   <div class="footer">
     <p>
-      &copy; Copyright 2023 All right reserved. CyberA Platform.
+      &copy; Copyright {{ currentYear }} All right reserved. CyberA Platform.
       <span style="color: white; font-size: 23px"
         ><i class="bi bi-arrow-up-square-fill"></i
       ></span>
